3926d3e037d5d0d2bc327a9929d6dc3e3b39fa00
Uma24112003 <sumamaheswari@903gmail.com>
2024-03-11 05:06:06 -0700

    update

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://hips.hearstapps.com/hmg-prod/images/edc030123mele-001-1675366859.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://interior-design-hazel.vercel.app"
